0ab818d76459a5faa99467cb8961fba9dc1c4446
andreybrsv <andreybrsv@ya.ru>
2020-07-18 23:38:55 +0300

    init sql + jdbc config

--- src/main/java/io/configuration/Config.java
+++ src/main/java/io/configuration/Config.java
@@ -1,8 +1,23 @@
 package io.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class Config {
 
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        return new NamedParameterJdbcTemplate(jdbcTemplate);
+    }
 }
