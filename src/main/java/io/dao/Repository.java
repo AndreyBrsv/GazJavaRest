1b4c2e5cd1ba37f46d9c61599bbac31cbfd4aab2
andreybrsv <andreybrsv@ya.ru>
2020-07-19 14:18:03 +0300

    javadoc add

--- src/main/java/io/dao/Repository.java
+++ src/main/java/io/dao/Repository.java
@@ -1,11 +1,32 @@
 package io.dao;
 
 public interface Repository<Entity, Identifier> {
+
+    /**
+     * Общий метод создания сущностей
+     * @param entity сущность
+     * @return созданную сущность
+     */
     Entity create(Entity entity);
 
+    /**
+     * Получение сущности по идентификатору
+     * @param identifier идентификатор
+     * @return сущность
+     */
     Entity get(Identifier identifier);
 
+    /**
+     * Обновление сущности
+     * @param entity сущность
+     * @return обновленная сущность
+     */
     Entity update(Entity entity);
 
+    /**
+     * Удаление сущности по индентификатору
+     * @param identifier идентификатор
+     * @return результат выполениния (true/false)
+     */
     boolean delete(Identifier identifier);
 }
--- src/main/java/io/service/DocumentService.java
+++ src/main/java/io/service/DocumentService.java
@@ -2,11 +2,30 @@ package io.service;
 
 import io.entities.Document;
 
+/**
+ * Сервисный слой для проверки и валидации входных параметров
+ * Использует DAO слой для работы с БД
+ */
 public interface DocumentService {
 
+    /**
+     * Создание документа
+     * @param document входной документ, для создания
+     * @return созданный документ
+     */
     Document create(Document document);
 
+    /**
+     * Поиск документа по номеру
+     * @param documentNumber номер документа
+     * @return найденный документ
+     */
     Document findByNumber(Long documentNumber);
 
+    /**
+     * Обновление полей документа
+     * @param document документ
+     * @return Обновленный документ
+     */
     Document update(Document document);
 }
